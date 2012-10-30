904b5e1aab7063713b7e60810038d752b271e85b
bruhn.markus@googlemail.com <bruhn.markus@googlemail.com@f55752d2-ed88-726a-c130-598517594cc4>
2012-10-30 11:19:52 +0000

    Merge implementiert, einfach mal zusammengehackt

---
+++ trunk/ad-praktikum-t13-t14/src/aufgabe2/algorithm/ExternerMergeSort.java
@@ -0,0 +1,49 @@
+package aufgabe2.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import aufgabe2.data.DataWrapper;
+
+public class ExternerMergeSort {
+	
+	
+	
+	// kopiert von /spielerei/Mergesort.java
+	
+	  private static DataWrapper merge(DataWrapper links, DataWrapper rechts) {
+	        int[] newList = new int[links.getSize()+rechts.getSize()];
+
+	        int linksPos=0;
+	        int rechtsPos=0;
+	        
+	        while ( linksPos < links.getSize() && rechtsPos < rechts.getSize()) 
+	        {
+	            int linksElem = links.getData()[linksPos];
+	            int rechtsElem = rechts.getData()[rechtsPos];
+	            
+	            if (linksElem <= rechtsElem) {
+	            	newList[linksPos + rechtsPos] = linksElem;
+	            	linksPos ++;
+	                
+	            } else {
+	            	newList[linksPos + rechtsPos] = rechtsElem;
+	            	rechtsPos ++;
+	            }
+	        }
+
+	        while (linksPos < links.getSize()) {
+	        	newList[linksPos + rechtsPos] = links.getData()[linksPos];
+            	linksPos ++;
+	        }
+
+	        while (rechtsPos < rechts.getSize()) {
+	        	newList[linksPos + rechtsPos] =  rechts.getData()[rechtsPos];
+            	rechtsPos ++;
+	        }
+
+	        return DataWrapper.create(newList, links.getSize()+rechts.getSize());
+
+	    }
+
+}
