44738c46ca6c9f03003cb289ba7225761bedb91e
sven.bartel90 <sven.bartel90@gmail.com>
2012-10-30 10:34:42 +0000

    t13: interfaces + DataManager create

---
+++ ad-praktikum-t13-t14/src/aufgabe2/interfaces/DataManager.java
@@ -0,0 +1,11 @@
+package aufgabe2.interfaces;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: abg667
+ * Date: 30.10.12
+ * Time: 11:32
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DataManager {
+}
