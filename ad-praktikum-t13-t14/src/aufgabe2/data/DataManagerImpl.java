2926539eed3da029f66c5b83f10ad9bd691f57c4
breitenstein.alexander <breitenstein.alexander@gmail.com>
2012-10-31 20:31:16 +0000

    t13: java.nio write, read int[]

--- ad-praktikum-t13-t14/src/aufgabe2/data/DataManagerImpl.java
+++ ad-praktikum-t13-t14/src/aufgabe2/data/DataManagerImpl.java
@@ -27,43 +27,57 @@ public class DataManagerImpl implements DataManager {
     DataWrapper write2;
 
 
-
-    FileChannel file1,file2,file3,file4;
+    FileChannel file1, file2, file3, file4;
     // ca 1.416.872 KB RAM  ( 357923000*4)
     private final int readerSize = 44740375;
-    private final int writerSize = readerSize*2;
+    private final int writerSize = readerSize * 2;
 
     private final String sourceFile = "zahlenfolge";
 
     public static void main(String[] args) {
         DataManager d = new DataManagerImpl();
-        while (true){
-
-        }
     }
+
     public DataManagerImpl() {
-        read1 = createDataWrapper(new int[readerSize],0);
-        read2 = createDataWrapper(new int[readerSize],0);
-        read3 = createDataWrapper(new int[readerSize],0);
-        read4 = createDataWrapper(new int[readerSize],0);
-        write1 = createDataWrapper(new int[writerSize],0);
-        write2 = createDataWrapper(new int[writerSize],0);
+//        read1 = createDataWrapper(new int[readerSize],0);
+//        read2 = createDataWrapper(new int[readerSize],0);
+//        read3 = createDataWrapper(new int[readerSize],0);
+//        read4 = createDataWrapper(new int[readerSize],0);
+//        write1 = createDataWrapper(new int[writerSize],0);
+//        write2 = createDataWrapper(new int[writerSize],0);
 
 
-
-
-         byte[] byteArray = new byte[4];
+        byte[] byteArray = new byte[4];
         byteArray[0] = 0;
         byteArray[1] = 0;
         byteArray[2] = 0;
         byteArray[3] = 1;
 
+        int[] intArray = new int[4];
+        intArray[0] = 0;
+        intArray[1] = 0;
+        intArray[2] = 0;
+        intArray[3] = 1;
 
+        writeIntArray(intArray);
 
-        IntBuffer intBuf = ByteBuffer.wrap(byteArray).order(ByteOrder.LITTLE_ENDIAN).asIntBuffer();
-        int[] array = new int[intBuf.remaining()];
-        intBuf.get(array);
-        System.out.println(array[0]);
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+
+        int[] intArray2 = new int[4];
+
+        intArray2 = readIntArray();
+
+        for (int i = 0; i < intArray2.length; i++) {
+            System.out.println(intArray2[i]);
+        }
+
+//        IntBuffer intBuf = ByteBuffer.wrap(byteArray).order(ByteOrder.BIG_ENDIAN).asIntBuffer();
+//        int[] array = new int[intBuf.remaining()];
+//        intBuf.get(array);
+//        System.out.println(array[0]);
+
 
 //        try {
 //            DataInputStream  instr = new DataInputStream(new BufferedInputStream(new FileInputStream( sourceFile ) ) );
@@ -72,9 +86,53 @@ public class DataManagerImpl implements DataManager {
 //            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
 //        }
 
+
     }
 
-    public DataWrapper createDataWrapper(int[] data, int size){
+    static int[] readIntArray() {
+        FileChannel fc = null;
+        try {
+            fc = new FileInputStream(new File("out.file")).getChannel();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        IntBuffer ib = null;
+        try {
+            ib = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size()).asIntBuffer();
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        int[] array = new int[4];
+        ib.get(array);
+        return array;
+    }
+
+    static void writeIntArray(int[] array) {
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream("out.file");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        try {
+            ByteBuffer byteBuff = ByteBuffer.allocate((Integer.SIZE / Byte.SIZE) * array.length);
+            IntBuffer intBuff = byteBuff.asIntBuffer();
+            intBuff.put(array);
+            intBuff.flip();
+            FileChannel fc = fos.getChannel();
+            fc.write(byteBuff);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+    }
+
+    public DataWrapper createDataWrapper(int[] data, int size) {
         return DataWrapperImpl.create(data, size);
     }
 
@@ -98,7 +156,8 @@ public class DataManagerImpl implements DataManager {
     public void write(DataWrapper dataWrapper) {
         //To change body of implemented methods use File | Settings | File Templates.
     }
-    private void fetchDataFromFile(DataWrapper whichReader,String filePath){
+
+    private void fetchDataFromFile(DataWrapper whichReader, String filePath) {
 
     }
 }
