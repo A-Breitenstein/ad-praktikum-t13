54a326aa9fea411dab36e99c61155f5cf560bb08
cschirin <cschirin@gmail.com>
2012-11-04 21:57:37 +0000

    Team 14: Der Merge-Schritt  verwendet jetzt die DataWrapper statt int-Arrays. Außerdem wurde MergeInputOutput (nicht vollständig) implementiert und die beiden Vorgänger-interfaces sind jetzt Deprecated. Ich weiß nicht, wie ich von innen heraus die Methoden GetNotCompleteMergedStream() und isBlockComplete() implementieren soll.

--- ad-praktikum-t13-t14/src/aufgabe2/interfaces/MergeInput.java
+++ ad-praktikum-t13-t14/src/aufgabe2/interfaces/MergeInput.java
@@ -1,20 +1,21 @@
 package aufgabe2.interfaces;
 
+@Deprecated
 public interface MergeInput {
 
     /**
      * Gibt die Elemente vom 1. Input zurück.
      *
-     * @return Array von sortierten Integer
+     * @return Verpackter Array von sortierten Integer
      */
-    int[] GetInput1();
+    DataWrapper GetInput1();
 	
     /**
      * Gibt die Elemente vom 2. Input zurück.
      *
-     * @return Array von sortierten Integer
+     * @return Verpackter Array von sortierten Integer
      */
-    int[] GetInput2();
+    DataWrapper GetInput2();
     
     /**
      * Ruft ab, Input1 und Input2 die letzten Elemente einer sortierten Blockgröße enthalten, sodass der Mergevorgang komplett erfolgen kann 
