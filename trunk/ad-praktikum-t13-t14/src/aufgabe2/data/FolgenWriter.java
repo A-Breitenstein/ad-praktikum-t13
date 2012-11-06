b17f1d76779ea1f7d6d409a852b4f1506bb199a4
sven.bartel90 <sven.bartel90@f55752d2-ed88-726a-c130-598517594cc4>
2012-11-06 23:33:02 +0000

    t13: FolgenWriter hotfix

--- trunk/ad-praktikum-t13-t14/src/aufgabe2/data/FolgenWriter.java
+++ trunk/ad-praktikum-t13-t14/src/aufgabe2/data/FolgenWriter.java
@@ -1,5 +1,7 @@
 package aufgabe2.data;
 
+import aufgabe2.interfaces.DataWrapper;
+
 import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
@@ -34,21 +36,67 @@ public class FolgenWriter {
         // bytebuffer als intbuffer interpretieren
         intBuffer = byteBuffer.asIntBuffer();
     }
+    public void close(){
+        // hier muss noch geguckt werden welches besser funzt...
+        if(intBuffer.position() != 0 || intBuffer.capacity() != intBuffer.remaining()){
+            writeBuffer();
+        }
+        fileWriter.close();
+    }
     private void writeBuffer(){
-        intBuffer.flip();
-        // warum byteBuffer? weil der intbuffer nur ne view auf den bytebuffer ist
-        fileWriter.writeByteBufferToFile(byteBuffer);
+        // wenn der intbuffer komplett voll ist schreib ihn so weg,
+        // sollte er nicht voll sein, dann MUSS der leere teil entfernt werden
+        // damit keine nullen in die datei geschrieben werden Oder sogar müll
+        // der sich noch im buffer befindet
+        if(intBuffer.remaining()==0){
+            intBuffer.flip();
 
+            // warum byteBuffer? weil der intbuffer nur ne view auf den bytebuffer ist
+            fileWriter.writeByteBufferToFile(byteBuffer);
+        }else{
+            // daten von dem zugroßen buffer (intBuffer) umschaufeln in den tmp_intbuffe um sie
+            // korekt wegschreiben zukönnen.
+            int positions = intBuffer.position();
+            ByteBuffer tmp_byteBuff = ByteBuffer.allocate((Integer.SIZE / Byte.SIZE) * positions);
+            IntBuffer tmp_intBuff = tmp_byteBuff.asIntBuffer();
+            intBuffer.flip();
+            for(int i= 0;i < positions;i++){
+                tmp_intBuff.put(intBuffer.get());
+            }
+            fileWriter.writeByteBufferToFile(tmp_byteBuff);
+            intBuffer.clear();
+
+        }
     }
 
 
-    public void writeFolge(int[] folge){
+    public void writeFolge(DataWrapper wrap){
+        // wenn die folgenLaenge gleich dem restlichem platz im buffer entspricht,
+        // dann du die folge rein und schreib sie raus
 
-        if(folgenLength > Writer.INTEGER_COUNT_PER_WRITE ){
-            multipleWritesPerFolge(folge);
-       }else {
-            singleWritesPerFolge(folge);
-       }
+        int[] folge = wrap.getData();
+        int folgenLaenge = wrap.getSize();
+        if(folgenLaenge == intBuffer.remaining()){
+            intBuffer.put(wrap.getData(),0,folgenLaenge);
+            writeBuffer();
+            intBuffer.clear();
+            // wenn die folgenLaenge kleiner ist dann einfach in den buffer rein
+        }else if(folgenLaenge < intBuffer.remaining()){
+            intBuffer.put(wrap.getData(),0,folgenLaenge);
+        }else {
+            // und wenn sie größer ist den rest auffüllen und dann den rest der folge
+            // in den geleerten buffer schreiben
+            // berechne den rest der folge der nicht in den buffer passt
+            int derRestDerNichtPasst = folgenLaenge - intBuffer.remaining();
+            // schreib den teil der "passt" in den buffer
+            intBuffer.put(folge,0,intBuffer.remaining());
+            writeBuffer();
+            intBuffer.clear();
+            // füll den rest der nicht passte nun in den intbuffer
+            for (int i = folgenLaenge-derRestDerNichtPasst; i < folgenLaenge ; i++) {
+                intBuffer.put(folge[i]);
+            }
+        }
     }
 
     private void multipleWritesPerFolge(int[] folge) {
