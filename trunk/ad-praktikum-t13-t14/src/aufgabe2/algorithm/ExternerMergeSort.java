f86696928c4c61c3b7f831e6a5671bbed58bd2e5
cschirin@googlemail.com <cschirin@googlemail.com@f55752d2-ed88-726a-c130-598517594cc4>
2012-11-06 16:24:28 +0000

    Algorithmus für morgen fertig Implementiert, zwei kleine "Buffer"-klassen hinzugefügt (für kompakteren code)

--- trunk/ad-praktikum-t13-t14/src/aufgabe2/algorithm/ExternerMergeSort.java
+++ trunk/ad-praktikum-t13-t14/src/aufgabe2/algorithm/ExternerMergeSort.java
@@ -1,5 +1,6 @@
 package aufgabe2.algorithm;
 
+import aufgabe2.algorithm.impl.*;
 import aufgabe2.data.DataManagerImpl;
 import aufgabe2.interfaces.*;
 
@@ -13,35 +14,25 @@ public class ExternerMergeSort {
 		//Blockweise Sortierung
 		DataWrapper data = tapes.readBlock(); //lese von "band" 1; initialisierung
 		while (data.getSize() > 0){ //solange das "band" nicht leer ist
-			tapes.write(blockSort(data)); // kleine blÃ¶cke sortieren
+			blockSort(data);//Sortieren
+			tapes.write(data); // zurückschreiben
 			data = tapes.readBlock(); //lese wieder von "band" 1
 		}
 		
-		/*
-		 * Code ohne DataWrapper, mit gewÃ¼nschter Implementation
-		
-		//Blockweise Sortierung
-		int[] data = tapes.readBlock(); //lese von "band" 1; initialisierung
-		while (data.length > 0){ //solange das "band" nicht leer ist
-			tapes.write(blockSort(data)); // kleine blÃ¶cke sortieren
-			data = tapes.readBlock(); //lese wieder von "band" 1
+		//Mergen
+		while(merge(tapes)){//Merge für jeden Block aufrufen, bis keine Blöcke mehr kommen
+			 //der merge tut schon alles, also do nothing
 		}
 		
-		//mergen
-		MergeInput mergeData;// = tapes.read();
-		while (mergeData.GetInput1().length>0 && mergeData.GetInput2().length>0){ //solange das "band" nicht leer ist
-			tapes.write(merge(mergeData)); // kleine blÃ¶cke sortieren
-			data = tapes.read(); //lese wieder von "band" 1
-		}
-		*/	
 	}
 	
-	private static DataWrapper blockSort(DataWrapper input){
+	private static void blockSort(DataWrapper data){
 		/* Das Verfahren zum internen sortieren der speicherblÃ¶cke
 		 *  benutzt momentan einen Insertionsort zum Sortieren.
+		 *  Arbeitet direkt auf dem dataWrapper  
 		 */
 		
-		int[] unpacked = input.getData();
+		int[] unpacked = data.getData();
 		
 		
 		for(int i = 1  ; i <= unpacked.length; i++ ) {
@@ -55,77 +46,85 @@ public class ExternerMergeSort {
 			unpacked[j] = itemToSort; 
 		}
 		
-		return input;
 	}
 	
-	public static int[] blockSort(int[] input){ // zum testen
-		/* Das Verfahren zum internen sortieren der speicherblÃ¶cke
-		 *  benutzt momentan einen Insertionsort zum Sortieren.
-		 *  
-		 *  Fï¿½r die zu sortierenden Int-Arrays muss eines gelten: 
-		 *  arr[0] ist ein Stopper-Element, d.h am besten die kleinste zahl ï¿½berhaupt.
-		 */
-		
-		int[] unpacked = input;
-		
-		
-		for(int i = 1 ; i < unpacked.length; i++ ) {
-			int j = i; 
-			int itemToSort = unpacked[i]; 
-			while(j>0 && unpacked[j-1] > itemToSort) { /* ohne stopper: while((unpacked[j-1] > itemToSort) && j > 1) */
-				
-				//insert
-				unpacked[j] = unpacked[j-1]; 
-				j = j-1; 
-			}
-			unpacked[j] = itemToSort; 
-		}
-		
-		return input;
-	}
-	
-	
-	  private static MergeInputOutput merge(MergeInputOutput input) {
-		  DataWrapper linksIn  = input.GetInput1();
-		  DataWrapper rechtsIn = input.GetInput2();
-          int[] links = linksIn.getData();
+	  private static boolean merge(DataManager ioTapes) {
+		  
+		  InputBuffer linksIn  = new InputBuffer(ioTapes, InputBuffer.Channels.LEFTCHANNEL);
+		  InputBuffer rechtsIn = new InputBuffer(ioTapes, InputBuffer.Channels.RIGHTCHANNEL);
+          
+		  OutputBuffer output = new OutputBuffer(ioTapes);
+		  
+		  if ((!linksIn.hasNext()) && (!rechtsIn.hasNext()))
+			  return false; //Keine weiteren Blöcke, die Sortiert werden könnten
+		  
+		  while (linksIn.hasNext() && rechtsIn.hasNext()){
+			  int linksElem = linksIn.getNext();
+	          int rechtsElem = rechtsIn.getNext();
+	          if (linksElem <= rechtsElem) {
+	        	  output.push(linksElem);
+	          } else {
+	        	  output.push(rechtsElem);
+	          }
+		  }
+		  
+		  while (linksIn.hasNext())
+			  output.push(linksIn.getNext());
+		  
+		  while (rechtsIn.hasNext())
+			  output.push(rechtsIn.getNext());
+		  
+		  output.storeInTape();
+		  
+		  return true;
+		  
+		  /*
+		  int[] links = linksIn.getData();
           int[] rechts = rechtsIn.getData();
 
-		  int linksLÃ¤nge = linksIn.getSize();
-		  int rechtsLÃ¤nge = rechtsIn.getSize();
+		  int linksLänge = linksIn.getSize();
+		  int rechtsLänge = rechtsIn.getSize();
 	      int linksPos=0;
 	      int rechtsPos=0;
-		  int[] merged = new int[linksLÃ¤nge + rechtsLÃ¤nge];
+	      int outputPos=0;
+		  DataWrapper mergedDW = inputTapes.createDataWrapper();
+	      int[] merged = mergedDW.getData();
+	      //if (mergedDW.getSize() == merged.length)
 	      
-		  while ( linksPos < linksLÃ¤nge && rechtsPos < rechtsLÃ¤nge) {
+		  while ( linksPos < linksLänge && rechtsPos < rechtsLänge) {
 			  int linksElem = links[linksPos];
 	          int rechtsElem = rechts[rechtsPos];
 	            
 	          if (linksElem <= rechtsElem) {
-	        	  merged[linksPos + rechtsPos] = linksElem;
-	        	  linksPos ++;  
+	        	  merged[outputPos] = linksElem;
+	        	  linksPos ++;
+	        	  outputPos ++;
+	        	  if (linksPos == linksLänge){
+	        		  linksIn = inputTapes.readLeftChannel();
+	        		  linksPos=0;
+	        		  links = linksIn.getData();
+	        		  linksLänge = linksIn.getSize();	  
+	        	  }
 	          } else {
-	        	  merged[linksPos + rechtsPos] = rechtsElem;
+	        	  merged[outputPos] = rechtsElem;
 	        	  rechtsPos ++;
+	        	  outputPos ++;
+	        	  if (rechtsPos == rechtsLänge){
+	        		  rechtsIn = inputTapes.readRightChannel();
+	        		  rechtsPos=0;
+	        		  rechts = rechtsIn.getData();
+	        		  rechtsLänge = rechtsIn.getSize();	        	  }
 	          }
+	          if(outputPos == merged.length){
+	        	  mergedDW.setSize(merged.length);
+	        	  inputTapes.write(mergedDW);
+	        	  mergedDW = null;
+	        	  mergedDW = inputTapes.createDataWrapper();
+	        	  outputPos = 0;
+	          }*/
+	        	  
 	      }
 
-		  if (input.isBlockComplete()) { //Nur wenn keine weiteren Daten zum gleichem Block kommen, Output komplett fÃ¼llen
-			  while (linksPos < linksLÃ¤nge) {
-				  merged[linksPos + rechtsPos] = links[linksPos];
-		          linksPos ++;
-		      }
-
-		      while (rechtsPos < rechtsLÃ¤nge) {
-		    	  merged[linksPos + rechtsPos] =  rechts[rechtsPos];
-		          rechtsPos ++;
-		      }
-		  }
-
-		  int NotMergedCount = (linksLÃ¤nge + rechtsLÃ¤nge) - (linksPos + rechtsPos);
-		  MergeInputOutput.InputStream NotCompleteMergedStream = ( rechtsPos < rechtsLÃ¤nge ? MergeInputOutput.InputStream.INPUT2 : MergeInputOutput.InputStream.INPUT1);
-	      
-		  return input; //wÃ¼nschenswerte fÃ¼r Erzeugung: MergeOutput.Create(merged, NotMergedCount, NotCompleteMergedStream);
-	    }
-
 }
+
+
