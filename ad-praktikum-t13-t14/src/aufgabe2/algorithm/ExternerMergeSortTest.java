adfe4ae55516a19707d648f5122d0046d5f33d34
bruhn.markus <bruhn.markus@googlemail.com>
2012-11-07 16:44:39 +0000

    Bugs beim Sortieren behoben, nun wird Quicksort genutzt

--- ad-praktikum-t13-t14/src/aufgabe2/algorithm/ExternerMergeSortTest.java
+++ ad-praktikum-t13-t14/src/aufgabe2/algorithm/ExternerMergeSortTest.java
@@ -1,12 +1,14 @@
 package aufgabe2.algorithm;
 
 import static junit.framework.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Arrays;
 
-import org.junit.Test;
+import org.junit.*;
 
 import aufgabe2.data.DWUtilityClass;
+import aufgabe2.data.TestFileGenerator;
 import aufgabe2.interfaces.DataWrapper;
 
 
@@ -40,7 +42,7 @@ public class ExternerMergeSortTest {
 	
 	@Test
 	public void testBlockSort_quick() {
-		int[] testelems = {6,3,7,2,8,7,345,8,323, 7,3,0,7,23,6,7,-4,546,34};
+		int[] testelems = {6,3,7,2,8,7,345,8,323, 7,3,0,7,23,6,7,-4,546,34,12};
 		int[] expectedArr =Arrays.copyOf(testelems,testelems.length); 
 		Arrays.sort(expectedArr); //Java-Standard-implementierung vom sortieren
 		
@@ -48,14 +50,16 @@ public class ExternerMergeSortTest {
 		DataWrapper expected = DWUtilityClass.createNewDataWrapper(expectedArr, expectedArr.length);
 		
 		ExternerMergeSort.blockSort_quick(test.getData(),0,testelems.length-1);
-		System.out.println(Arrays.toString(test.getData()));
+		//System.out.println(Arrays.toString(test.getData()));
 		assertEquals(expected, test); 
 	}
 	
 	
-	@Test 
+	@Test //@Ignore 
 	public void testMergeSortAlgorithm() {
-		
+		TestFileGenerator.createTestFile("DataManagerTest",100,10);
+        ExternerMergeSort.sort("DataManagerTest","");
+        assertTrue(TestFileGenerator.isSorted("DataManagerTest"));
 	}
 
 }
