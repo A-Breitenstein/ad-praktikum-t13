cd89f30d5ad4a4c983c2d449bc2be4e9508da29c
bruhn.markus <bruhn.markus@googlemail.com>
2012-11-23 12:58:53 +0000

    Etwas mehr Übersicht... Mehr habe ich eben nicht geschafft.

--- ad-praktikum-t13-t14/src/aufgabe2/data/Constants.java
+++ ad-praktikum-t13-t14/src/aufgabe2/data/Constants.java
@@ -15,41 +15,41 @@ final class Constants {
 	/**
 	 * Die Größe eines Integers in Bytes
 	 */
-	public static final long INTSIZE = 4; // ein Int benötigt 4 Bytes
+	public static final int INTSIZE = 4; // ein Int benötigt 4 Bytes
 	
 	
 	/**
 	 * Die (maximale) Größe des Speichers in Bytes, der für das Arbeiten 
 	 * auf dem Array genutzt werden darf.
 	 */
-	public static final long BUFFERSIZE_SORTARRAY = toValidIntSize((long)(BUFFERSIZE_APPLICATION )); //Gesamten Arbeitsspeicher verwenden!
+	public static final int BUFFERSIZE_SORTARRAY = toValidIntSize(BUFFERSIZE_APPLICATION); //Gesamten Arbeitsspeicher verwenden!
 	/**
 	 * Die Größe der Runs beim ersten Durchlauf (=Sortieren)
 	 */
-	public static final int INITBLOCKINTEGERS = (int)(BUFFERSIZE_SORTARRAY / 4);
+	//public static final int INITBLOCKINTEGERS = (int)(BUFFERSIZE_SORTARRAY / 4);
 			
 	
 	/**
 	 * Die (maximale) Größe des Speichers in Bytes, der für einen LeseVorgang pro Datei und Tread
 	 * genutzt werden darf
 	 */
-	public static final long BUFFERSIZE_MERGEREAD = toValidIntSize(BUFFERSIZE_APPLICATION / 8); //Anmerkung: es gibt zwei lesende Dateien und zwei Treads (Rechentread/ IO-Tread)
+	public static final int BUFFERSIZE_MERGEREAD = toValidIntSize(BUFFERSIZE_APPLICATION / 8); //Anmerkung: es gibt zwei lesende Dateien und zwei Treads (Rechentread/ IO-Tread)
 	/**
 	 * Die (maximale) Größe des Speichers in Bytes, der für einen Schreibvorgang pro Datei und
 	 * Thread genutzt werden darf
 	 */
-	public static final long BUFFERSIZE_MERGEWRITE = toValidIntSize(BUFFERSIZE_APPLICATION / 8); //Anmerkung: es gibt zwei schreibende Dateien (auch, wenn abwechselnd in diese geschrieben werden) und zwei Treads (Rechentread/ IO-Tread)
+	public static final int BUFFERSIZE_MERGEWRITE = toValidIntSize(BUFFERSIZE_APPLICATION / 8); //Anmerkung: es gibt zwei schreibende Dateien (auch, wenn abwechselnd in diese geschrieben werden) und zwei Treads (Rechentread/ IO-Tread)
 	
 	
 	
 	/**
 	 * Berechnet aus dem gegebenen Maximalen Speicher den maximalen Anteil, in welchen sich
-	 * Integer-Zahlen dargestellt werden können 
+	 * Integer-Zahlen dargestellt werden können, maximal INTEGER.MAXVALUE
 	 * @param maxBufferSize
 	 * @return int - Postcondition: return % INTSIZE == 0 && returnvalue + INTSIZE > maxBufferSize
 	 */
-	private static long toValidIntSize(long maxBufferSize){
-		return (long)(maxBufferSize / INTSIZE) * INTSIZE;
+	private static int toValidIntSize(long maxBufferSize){
+		return ((int)Math.min(Integer.MAX_VALUE,maxBufferSize) / INTSIZE) * INTSIZE;
 	}
 	
 	
